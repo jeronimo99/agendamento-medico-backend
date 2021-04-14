1e72a1edd108dd5b90e01f5b046cc94d7b3b4d23
Jeronimo Ramalho <jeronimo.ramalho@outlook.com>
2021-04-14 00:29:45 -0300

    Update tables design

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { body } = require('express-validator');
+const router = Router();
+
+const {registerController} = require('../controllers/auth');
+
+router.post('/user/register', body('username').isEmail().isLowercase(), body('password').isLength({ min: 6, max: 20 }), registerController);
+router.post('/user/delete', body('username').isEmail().isLowercase(), body('password').isLength({ min: 6, max: 20 }), registerController);
+
+module.exports = router;
